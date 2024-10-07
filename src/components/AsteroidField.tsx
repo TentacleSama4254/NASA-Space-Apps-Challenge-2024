f1ca9fba97e82c4de13cc7c199bb46585f1b007a
Inusha De Silva <74726248+TentacleSama4254@users.noreply.github.com>
2024-10-07 01:25:36 +0000

    tftghgc

--- src/components/AsteroidField.tsx
+++ src/components/AsteroidField.tsx
@@ -1,6 +1,7 @@
 // src/components/AsteroidField.tsx
 import React, { useEffect, useState } from "react";
 import Asteroid from "./Asteroid";
+import Revolution from "./Revolution";
 
 const AsteroidField = () => {
   const [asteroids, setAsteroids] = useState<any[]>([]);
@@ -15,20 +16,42 @@ const AsteroidField = () => {
 
     fetchData();
   }, []);
+    
+    
 
   return (
     <>
-      {asteroids.slice(0, 20).map((asteroid, index) =>
-        // console.log(asteroid)
-        <Asteroid
-          key={index}
-              name={asteroid.name}
-              scale={asteroid.scale}
-              texture_path=""
-        //   diameter={asteroid.diameter}
-          position={[asteroid.a, asteroid.e, asteroid.i]} // Example mapping
-          
-        />
+      {asteroids.slice(0, 20).map(
+        (asteroid, index) => (
+          // console.log(asteroid)
+          <Revolution
+            Component={Asteroid}
+            key={`asteroid_${index}`}
+            componentProps={{
+              name: asteroid.name,
+              scale: asteroid.scale,
+              texture_path: asteroid.texture_path,
+              position: [asteroid.a, asteroid.e, asteroid.i],
+            }}
+            orbit={{
+              a: asteroid.a,
+              e: asteroid.e,
+              inclination: asteroid.i,
+              omega: asteroid.omega,
+              raan: asteroid.raan,
+              q: asteroid.q,
+            }}
+          />
+        )
+        // <Asteroid
+        //   key={index}
+        //       name={asteroid.name}
+        //       scale={asteroid.scale}
+        //       texture_path=""
+        // //   diameter={asteroid.diameter}
+        //   position={[asteroid.a, asteroid.e, asteroid.i]} // Example mapping
+
+        // />
         //   null
       )}
     </>
